30f5dd60fb53fe4bc1d4417c6839970efc5d6e91
Prarthit Patel <prarthitpatel1010@gmail.com>
2025-07-03 08:48:31 +0530

    dynamic data on signup API

--- src/app.js
+++ src/app.js
@@ -7,15 +7,15 @@ const User = require("./Model/user")
 
 const app = express();
 
+app.use(express.json());
 
-app.post("/signup", async (err,req,res)=>{
 
-    const user = new User({
-        firstName : "Prarthit",
-        lastName : "Patel",
-        emailId :"prarthit12@gmail.com",
-        mobileNumber : 12355445885
-    })
+
+
+app.post("/signup", async (req,res)=>{
+
+   
+    const user = new User(req.body)
 
 
     try{
@@ -28,7 +28,7 @@ app.post("/signup", async (err,req,res)=>{
     }
     catch(err){
 
-        res.status(400).send("BAd request" + err.message);
+       
     }
 
 })
